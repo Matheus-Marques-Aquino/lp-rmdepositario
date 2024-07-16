5869aefcde481ca4c405baf02c781abe38db5374
Allison Yuri <40338591+TheZehel@users.noreply.github.com>
2024-07-16 16:03:53 -0300

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Link from 'next/link';
+import Link from "next/link";
 import { useState, useEffect } from "react";
 
 import Form from "../components/Form";
@@ -17,7 +17,7 @@ import { IoIosMail } from "react-icons/io";
 import { FaPhoneAlt } from "react-icons/fa";
 
 export default function Home() {
-  const [selectValue, setSelectValue] = useState('');
+  const [selectValue, setSelectValue] = useState("");
 
   useEffect(() => {
     document.title = "RM Depositário";
@@ -25,34 +25,31 @@ export default function Home() {
 
   const onSelectChange = (value) => {
     setSelectValue(value);
-  }
+  };
 
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth' // Para uma rolagem suave
+      behavior: "smooth", // Para uma rolagem suave
     });
   };
 
   return (
     <main className="bg-white w-full">
-      <div className="h-[60px] border-b-[1px] border-b-[#000] border-opacity-10 flex shadow-lg bg-[#212121] sticky top-0 z-[1000] " >
+      <div className="h-[60px] border-b-[1px] border-b-[#000] border-opacity-10 flex shadow-lg bg-[#212121] sticky top-0 z-[1000] ">
         <div className="w-full max-w-[1000px] h-full mx-auto flex">
           <div className="w-[130px] mx-[15px] my-auto flex">
-            <img 
-              src={"/imgs/Logo-RM.png"}
-              className="w-auto h-auto my-auto"
-            />
+            <img src={"/imgs/Logo-RM.png"} className="w-auto h-auto my-auto" />
           </div>
           <div className="w-fit my-auto text-white ml-auto flex">
             <div>
               <div className="font-normal text-[15px] sm:text-[12px] leading-[20px] flex">
-                  <FaPhoneAlt className="w-[18px] h-[13px] mr-[5px] my-auto"/>
-                  <div>(11) 9.8874-6387</div>
+                <FaPhoneAlt className="w-[18px] h-[13px] mr-[5px] my-auto" />
+                <div>(11) 9.8874-6387</div>
               </div>
               <div className="font-normal text-[12px] sm:text-[12px] leading-[20px] mt-[0px] flex">
-                  <FaPhoneAlt className="w-[18px] h-[13px] mr-[5px] my-auto"/>
-                  <div>(11) 9.9973-1913</div>
+                <FaPhoneAlt className="w-[18px] h-[13px] mr-[5px] my-auto" />
+                <div>(11) 9.9973-1913</div>
               </div>
             </div>
             <div className="font-normal text-[12px] sm:text-[12px] leading-[20px] flex cursor-pointer ml-5">
@@ -62,36 +59,39 @@ export default function Home() {
               </div>
             </div>
           </div>
-        </div>        
+        </div>
       </div>
-      <div className="w-full flex py-[40px] bg-[#FFFFFF] bg-[url('/imgs/background-1.png')] bg-cover bg-center bg-no-repeat" >
+      <div className="w-full flex py-[40px] bg-[#FFFFFF] bg-[url('/imgs/background-1.png')] bg-cover bg-center bg-no-repeat">
         <div className="w-full max-w-[1000px] mx-auto lg:flex">
-            <div className="w-fit h-fit mt-auto mb-[40px] mx-auto px-[8px] lg:mr-auto lg:ml-0 xs:px-0">
-              <HeaderMessage/>              
-            </div>   
-            <div className="w-fit my-auto mx-auto px-[8px] lg:mx-0 xs:px-0">
-              <Form
-                selectValue={selectValue}
-                onSelectChange={onSelectChange}
-              />
-            </div>     
+          <div className="w-fit h-fit mt-auto mb-[40px] mx-auto px-[8px] lg:mr-auto lg:ml-0 xs:px-0">
+            <HeaderMessage />
+          </div>
+          <div className="w-fit my-auto mx-auto px-[8px] lg:mx-0 xs:px-0">
+            <Form selectValue={selectValue} onSelectChange={onSelectChange} />
+          </div>
         </div>
       </div>
       <div className="w-full z-10">
-        <FirstRow/>
+        <FirstRow />
         <SecondRow
-          updateSelect={(value)=>{ onSelectChange(value); scrollToTop(); }}
+          updateSelect={(value) => {
+            onSelectChange(value);
+            scrollToTop();
+          }}
         />
-        <AboutUs/>
-        <NewsLetter/>
-        <Footer/>
+        <AboutUs />
+        <NewsLetter />
+        <Footer />
       </div>
-      <Link href="https://api.whatsapp.com/send/?phone=5511972482006" rel="noopener noreferrer" target="_blank">
+      <Link
+        href="https://api.whatsapp.com/send/?phone=11988746387"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
         <div className="fixed bottom-[25px] right-[25px] z-50 sm:w-[75px] sm:h-[75px] w-[60px] h-[60px] rounded-full bg-[#25D366] flex cursor-pointer hover:opacity-80 transition ease-in-out duration-500">
-          <FaWhatsapp className="w-[40px] h-[40px] sm:w-[54px] sm:h-[54px] m-auto text-white"/>
-        </div>        
+          <FaWhatsapp className="w-[40px] h-[40px] sm:w-[54px] sm:h-[54px] m-auto text-white" />
+        </div>
       </Link>
-
     </main>
   );
 }
