41e950a8729a2060034f78fe0ff04ec0f35060e7
Allison Yuri <40338591+TheZehel@users.noreply.github.com>
2024-07-26 18:03:49 -0300

    Update index.js

--- components/media/index.js
+++ components/media/index.js
@@ -7,17 +7,21 @@ export default function Media() {
     <div className="flex flex-col md:flex-row m-6">
       <div className="w-full md:w-1/3 rounded-md">
         <img
-          className="w-full mb-4 md:mb-0 rounded-md"
+          className="w-full mb-4 md:mb-0 rounded-md p-1"
           src={img1Url}
           alt="Imagem 1"
         />
       </div>
       <div className="w-full md:w-1/3 rounded-md">
-        <video className="w-full rounded-md" src={videoUrl} controls></video>
+        <video
+          className="w-full rounded-md p-1"
+          src={videoUrl}
+          controls
+        ></video>
       </div>
       <div className="w-full md:w-1/3 rounded-md">
         <img
-          className="w-full mt-4 md:mt-0 rounded-md"
+          className="w-full mt-4 md:mt-0 rounded-md p-1"
           src={img2Url}
           alt="Imagem 2"
         />
