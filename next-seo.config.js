c9ec1145387bd3432ba6feb6443bc40337890b07
Allison Yuri <40338591+TheZehel@users.noreply.github.com>
2025-01-06 09:42:19 -0300

    Update - SEO

---
+++ next-seo.config.js
@@ -0,0 +1,14 @@
+const SEOConfig = {
+  title: 'RM Depositário | Depositário Judicial',
+  description:
+    'Fazemos contrato exclusivo para prestação de serviço em todo Brasil',
+  openGraph: {
+    type: 'website',
+    url: 'https://rmdepositario.com/',
+    title: 'RM Depositário | Depositário Judicial',
+    description:
+      'Fazemos contrato exclusivo para prestação de serviço em todo Brasil',
+  },
+};
+
+export default SEOConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import "@/styles/globals.css";
+import { DefaultSeo } from 'next-seo'; // Corrige a importação do DefaultSeo
+import SEOConfig from '@/next-seo.config'; // Importa o arquivo de configuração corretamente
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <DefaultSeo {...SEOConfig} />
+      <Component {...pageProps} />
+    </>
+  );
 }
